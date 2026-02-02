5b48e9b5be1227c70900817c84da58f72273b25e
djoe73660-blip <djoe73660@gmail.com>
2026-02-02 03:18:08 +0100

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -4,8 +4,10 @@ require("dotenv").config();
 // Use a dummy key if the secret isn't loaded yet to prevent HHE3 crash
 const P_KEY = process.env.PRIVATE_KEY || "0000000000000000000000000000000000000000000000000000000000000000";
 
-module.exports = {
+ module.exports = {
   solidity: "0.8.20",
+   // ... the rest of your config
+};
   networks: {
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL || "",
