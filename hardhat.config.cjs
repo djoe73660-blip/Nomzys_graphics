146cf3282d1c818910f6a83ce02ec629e4cc3200
djoe73660-blip <djoe73660@gmail.com>
2026-02-02 06:37:17 +0100

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,7 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
     version: "0.8.20",
