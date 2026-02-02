0ff668fb99cba68c3db1823263a66773e52163d0
djoe73660-blip <djoe73660@gmail.com>
2026-02-02 03:57:31 +0100

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,24 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+
+ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-// Use a dummy key if the secret isn't loaded yet to prevent HHE3 crash
-const P_KEY = process.env.PRIVATE_KEY || "0000000000000000000000000000000000000000000000000000000000000000";
-
- module.exports = {
+// Use module.exports for .cjs files
+module.exports = {
   solidity: "0.8.20",
-   // ... the rest of your config
-};
   networks: {
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL || "",
-      accounts: [P_KEY],
-    },
-    polygon: {
-      url: process.env.POLYGON_RPC_URL || "",
-      accounts: [P_KEY],
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     }
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY || ""
   }
 };
